f1cf523bbdf77fc5df8f0b892ad339da50541116
toby <toby@swinson-howell.net>
2022-08-25 07:12:23 +0100

    Initial commit

---
+++ forms.js
@@ -0,0 +1,6 @@
+const queryString = window.location.search;
+console.log(queryString);
+// ?product=shirt&color=blue&newuser&size=m
+
+const urlParams = new URLSearchParams(queryString);
+urlParams.get()
